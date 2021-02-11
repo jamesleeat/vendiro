0beb03fbd573885b733c7a847aa914d1665ea77e
James Archbold <jamesleeat@gmail.com>
2021-02-11 11:17:45 -0500

    First message

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      primary: "#35B091",
+      secondary: "#F8F8F8",
+      white: "white",
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
