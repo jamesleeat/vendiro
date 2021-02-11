2c5af62ad56942731ef693bf02c86ca444b9b9a3
James Archbold <jamesleeat@gmail.com>
2021-02-11 14:38:29 -0500

    Finished version 1 and version 2

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,36 @@
+const options = {
+  columns: ["Product 1", "Product 2", "Product 3"],
+  methods: ["Method 1", "Method 2"],
+};
+
+const filters = [];
+
 $(document).ready(function () {
   $(".dropdown-btn").click(function () {
-    $("#filterFormDropdown").toggle();
+    $("#filterFormDropdown").fadeToggle();
   });
   $(".close-btn").click(function (e) {
     e.preventDefault();
-    $("#filterFormDropdown").toggle();
+    $("form").trigger("reset");
+    $("#filterFormDropdown").fadeToggle();
+  });
+  $(".add-btn").click(function (e) {
+    e.preventDefault()
+    const formData = $("form")
+      .serializeArray()
+      .reduce(function (a, x) {
+        a[x.name] = x.value;
+        return a;
+      }, {});
+    filters.push(formData);
+    console.log(filters);
+    // $("#filterFormDropdown").toggle();
+  });
+
+  options.columns.forEach((value) => {
+    $("#column").append(`<option value="${value}">${value}</option>`);
+  });
+  options.methods.forEach((value) => {
+    $("#method").append(`<option value="${value}">${value}</option>`);
   });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,22 @@
+const colors = {
+  primary: "#35B091",
+  secondary: "#F8F8F8",
+  white: "white",
+};
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      primary: "#35B091",
-      secondary: "#F8F8F8",
-      white: "white",
+      primary: colors.primary,
+      secondary: colors.secondary,
+      white: colors.white,
     },
-    extend: {},
+    extend: {
+      outline: {
+        primary: `1px solid ${colors.primary}`,
+      },
+    },
   },
   variants: {
     extend: {},
